fadde10c41fd6ff971defea91a8d29955adfc4cb
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-10-18 14:25:57 -0300

    Loading models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => res.json({ ok: true }));
+routes.get('/teste', async (req, res) => {
+  const user = await User.create({
+    name: 'Grazziano',
+    email: 'grazzianofagundes@gmail.com',
+    password_hash: '123123',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
