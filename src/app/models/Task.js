ca90acab39925bf4d6c60ea47a94bde4dd6366e5
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2023-10-22 21:12:30 -0300

    Migration and task model

---
+++ src/app/models/Task.js
@@ -0,0 +1,30 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Task extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        task: Sequelize.STRING,
+        check: Sequelize.BOOLEAN,
+        createdAt: {
+          field: 'created_at',
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          field: 'updated_at',
+          type: Sequelize.DATE,
+        },
+      }, {
+      sequelize,
+    }
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+  }
+
+}
+
+export default Task
